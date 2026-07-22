ce0834cc0c8b2a6be460fcd32fd719276eb72dd4
Etienne Robert <etiennerobert33@gmail.com>
2026-07-22 22:04:08 +0200

    chore(nix): add flake setup with devshell and nodejs pnpm dependencies

---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+  outputs =
+    { self, nixpkgs }:
+    let
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "aarch64-darwin"
+      ];
+
+      forAllSystems = nixpkgs.lib.genAttrs systems;
+    in
+    {
+      devShells = forAllSystems (
+        system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in
+        {
+          default = pkgs.mkShell {
+            packages = [
+              pkgs.nodejs
+              pkgs.pnpm
+            ];
+          };
+        }
+      );
+    };
+}
